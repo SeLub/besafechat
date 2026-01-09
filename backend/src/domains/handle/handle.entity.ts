5dd747cae197fbe1982e7c031a6b4c5a05674e8e
selub <seport24@list.ru>
2026-01-09 11:28:48 +0300

    refactor: remove extra endpoints & add alias unique check

--- backend/src/domains/handle/handle.entity.ts
+++ backend/src/domains/handle/handle.entity.ts
@@ -36,7 +36,7 @@ export class Handle {
   })
   type!: HandleType;
 
-  @Column({ type: 'varchar', length: 255, unique: true, nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
   alias?: string | null;
 
   @Column({ type: 'boolean', default: false })
